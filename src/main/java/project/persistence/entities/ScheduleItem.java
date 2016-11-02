6d5ab924d2d3fd40d893dc660b10f4778d900ac5
thorunnarna <thorunnarna93@gmail.com>
2016-11-02 17:40:03 +0000

    scheduleitem get/ste

--- src/main/java/project/persistence/entities/ScheduleItem.java
+++ src/main/java/project/persistence/entities/ScheduleItem.java
@@ -1,4 +1,6 @@
 package project.persistence.entities;
+import java.time.*;
+import java.util.*;
 
 /**
  * Created by Þórunn on 11/2/2016.
@@ -7,15 +9,58 @@ public class ScheduleItem {
     private String title;
     private int id;
     private int userId;
-    //private DateTime  Starttime;
-    private User[] taggedUsers;
-    //private datetime endTime;
+    private LocalDate startTime;
+    private List<User> taggedUsers;
+    private LocalDate endTime;
     private int weekNo;
     private int year;
     private String location;
     private String color;
     private String description;
-    private String[] filters;
+    private List<String> filters;
+    private String filter;
+
+    public String getTitle(){return title;}
+    public void setTitle(String title){this.title = title;}
+
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
+
+    public int getUserId(){return userId;}
+    public void setUserId(int userId){this.userId = userId;}
+
+    public LocalDate getStartTime(){return startTime;}
+    public void setStartTime(LocalDate startTime){this.startTime = startTime;}
+
+    public LocalDate getEndTime(){return endTime;}
+    public void setEndTime(LocalDate endTime){this.endTime = endTime;}
+
+    public List<User> getTaggedUsers(){return taggedUsers;}
+    public void addTaggeduser(User user){taggedUsers.add(user);}
+    public void deleteTaggedUser(User user){taggedUsers.remove(user);}
+
+    public int getWeekNo(){return weekNo;}
+    public void setWeekNo(int weekNo){this.weekNo = weekNo;}
+
+    public int getYear(){return year;}
+    public void setYear(int year){this.year = year;}
+
+    public String getLocation(){return location;}
+    public void setLocation(String location ){this.location = location;}
+
+    public String getColor(){return color;}
+    public void setColor(String color ){this.color = color;}
+
+    public String getDexcription(){return description;}
+    public void setDescription(String description ){this.description = description;}
+
+    public List<String> getfilters(){return filters;}
+    public void addFilter(String filter){filters.add(filter);}
+    public void deleteFilter(String filter){filters.remove(filter);}
 
 
+
+    public void changeTime(LocalTime start, LocalTime end){}
+
+    public void delete(){}
 }
