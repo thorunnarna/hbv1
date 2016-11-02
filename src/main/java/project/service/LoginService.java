9a33abdbac67c8ba6429eb10746196871d7cee87
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2016-11-02 18:48:02 +0000

    LoginService

--- src/main/java/project/service/LoginService.java
+++ src/main/java/project/service/LoginService.java
@@ -1,7 +1,16 @@
 package project.service;
 
+import org.springframework.ui.Model;
+import project.persistence.entities.*;
+import project.persistence.repositories.Repository;
+
 /**
  * Created by halld on 02-Nov-16.
  */
 public class LoginService {
+
+    Repository repository;
+
+    public void createUser(User user) {}
+    public void loginUser(int userId){}
 }
