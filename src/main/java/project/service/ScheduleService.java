d7fd7f9d10f2cc33ba02ad0c7f2dc0bb66fbdd07
thorunnarna <thorunnarna93@gmail.com>
2016-11-03 18:34:08 +0000

    smá scheduleService

--- src/main/java/project/service/ScheduleService.java
+++ src/main/java/project/service/ScheduleService.java
@@ -1,9 +1,14 @@
 package project.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.persistence.entities.ScheduleItem;
+import project.persistence.entities.User;
 import project.persistence.repositories.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by halld on 02-Nov-16.
@@ -19,19 +24,28 @@ public class ScheduleService {
         repository = new Repository();
     }
 
-    public void addItem(int itemId){
+    public void addItem(int itemId, int userId){
+
+        repository.
+
 
     }
 
-    public void deleteItem(int itemId){
+    public void deleteItem(int itemId, int userId){
+
+    }
+    //adda úr sínu scheduli?
+    public void removeItem(int itemId,int userId){
 
     }
 
-    public void removeItem(int itemId){
+    public ScheduleItem createItem(String title, int userId, LocalDate startTime, LocalDate endTime,
+                                   List<User> taggedUsers, int weekNo, int year, String location,String color,
+                                   String description, List<String> filters){
+        repository.createItem()
 
-    }
 
-    public void createItem(ScheduleItem scheduleItem){
+
 
     }
 }
