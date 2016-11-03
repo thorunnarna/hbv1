cefe65a446721b4939ec37f617c23b8b28d1ae33
thorunnarna <thorunnarna93@gmail.com>
2016-11-03 17:47:13 +0000

    user entity

--- src/main/java/project/persistence/entities/User.java
+++ src/main/java/project/persistence/entities/User.java
@@ -1,34 +1,36 @@
 package project.persistence.entities;
 
+import java.util.List;
+
 /**
  * Created by Þórunn on 11/2/2016.
  */
 public class User {
+    private List<User> friends;
     private String username;
     private int userId;
-    private String password;
     private String photo;
     private String school;
 
-    public String getUsername(){return username;};
+    public List<User> getFriends(){return friends;}
+    public void addFriend(User user){friends.add(user);}
+    public void deleteFriend(User user){friends.remove(user);}
+
+    public String getUsername(){return username;}
     public void setUsername(String username){this.username = username;}
 
-    public int getUserId(){return userId;};
-    public void setUserId(int userId){this.userId = userId;};
+    public int getUserId(){return userId;}
+    public void setUserId(int userId){this.userId = userId;}
 
-    public String getPassword(){return password;};
-    public void setPassword(String password){this.password = password;};
+    public String getPhoto(){return photo;}
+    public void setPhoto(String photo){this.photo = photo;}
 
-    public String getPhoto(){return photo;};
-    public void setPhoto(String photo){this.photo = photo;};
-
-    public String getSchool(){return school;};
-    public void setSchool(String school){this.school = school;};
+    public String getSchool(){return school;}
+    public void setSchool(String school){this.school = school;}
 
 
-    public void changePassword(String oldpass, String newpass){};
 
-    public int addFriend(int userId){return 0;};
+
 
 
 
