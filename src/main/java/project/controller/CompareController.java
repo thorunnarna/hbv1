93fb9fff33660c96cb1373b5f0072390201fd63e
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2016-11-06 14:04:46 +0000

    SearchController og CompareController

--- src/main/java/project/controller/CompareController.java
+++ src/main/java/project/controller/CompareController.java
@@ -3,6 +3,7 @@ package project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,17 +30,17 @@ public class CompareController {
     }*/
 
     @RequestMapping(value="/compare/{userId1}/{userId2}", method = RequestMethod.POST)
-    public String compareSchedulePost(Model model, @PathVariable("userId1") int user1, @PathVariable("userId2")
+    public String compareSchedulePost(@ModelAttribute("comparedSchedule") Schedule comparedSchedule, Model model, @PathVariable("userId1") int user1, @PathVariable("userId2")
             int user2, int weekNo, int year){
         Schedule schedule = compareService.compareSchedules(user1, user2, weekNo, year);
-        model.addAttribute(schedule);
+        model.addAttribute("comparedSchedule", schedule);
         return "";
     }
 
     @RequestMapping(value="/compare/{groupId}", method = RequestMethod.POST)
-    public String compareGroupSchedulePost(Model model, @PathVariable("groupId") int grpId, int weekNo, int year){
+    public String compareGroupSchedulePost(@ModelAttribute("comparedGroupSchedule") Schedule comparedGroupSchedule,Model model, @PathVariable("groupId") int grpId, int weekNo, int year){
         Schedule groupSchedule = compareService.compareScheduleGroup(grpId, weekNo, year);
-        model.addAttribute(groupSchedule);
+        model.addAttribute("comparedGroupSchedule", groupSchedule);
         return "";
     }
 
--- src/main/java/project/controller/SearchController.java
+++ src/main/java/project/controller/SearchController.java
@@ -3,6 +3,7 @@ package project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +26,7 @@ public class SearchController {
         this.searchService = searchService;
     }
 
-    @RequestMapping(value="", method = RequestMethod.GET)
+    @RequestMapping(value="/user/all", method = RequestMethod.GET)
     public String viewGetListOfUsers(Model model){
 
         List<User> users = searchService.findAll();
@@ -34,8 +35,8 @@ public class SearchController {
         return "/";
     }
 
-    @RequestMapping(value="", method = RequestMethod.GET)
-    public String getSearchByName(String username, Model model){
+    @RequestMapping(value="/user/{username}", method = RequestMethod.GET)
+    public String getSearchByName(@PathVariable("username") String username, Model model){
 
         User user = searchService.findByName(username);
         model.addAttribute(user);
@@ -43,9 +44,9 @@ public class SearchController {
         return "/";
     }
 
-    @RequestMapping(value="", method = RequestMethod.POST)
-    public String addFriendPost(@ModelAttribute("friendship") User friendship,
-                                Model model, int userId1, int userId2){
+    @RequestMapping(value="/user/{userId1}/{userId2}", method = RequestMethod.POST)
+    public String addFriendPost(@ModelAttribute("friendship") Boolean friendship,
+                                Model model, @PathVariable("userId1") int userId1, @PathVariable("userId2") int userId2){
 
         model.addAttribute("friendship", searchService.createFriendship(userId1,userId2));
 
