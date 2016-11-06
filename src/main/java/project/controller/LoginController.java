79dd777588fcba6f03788d4f6eca2d268812e9a2
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2016-11-06 15:26:47 +0000

    LoginController, signUpPost :santa:

--- src/main/java/project/controller/LoginController.java
+++ src/main/java/project/controller/LoginController.java
@@ -3,6 +3,9 @@ package project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import project.persistence.entities.User;
 import project.service.LoginService;
 
@@ -20,12 +23,14 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    public String signUpPost(Model model, User user){
-        //loginService.createUser();
-        //model.addAttribute("blaaaa", loginService.???);
-        return "";
+    @RequestMapping(value="/signUp", method = RequestMethod.POST)
+    public String signUpPost(@ModelAttribute("SignUp") User Signup, Model model, String username, String password, String photo, String school) {
+        User user = loginService.createUser(username, password, photo, school);
+        model.addAttribute("Signup", user);
+        return "/";
     }
 
+    @RequestMapping(value="/", method = RequestMethod.POST)
     public String loginUserPost(String user, String pass, Model model){
         return "";
     }
