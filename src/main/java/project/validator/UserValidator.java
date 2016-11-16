d0371dfedcb7e012054ea05762228ecc7a56b1b5
svava <svavahb@gmail.com>
2016-11-16 17:28:38 +0000

    Validator kominn

---
+++ src/main/java/project/validator/UserValidator.java
@@ -0,0 +1,44 @@
+package project.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import project.persistence.entities.User;
+import project.service.LoginService;
+
+/**
+ * Created by Svava on 16.11.16.
+ */
+public class UserValidator implements Validator {
+
+    @Autowired
+    private LoginService loginService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (user.getUsername().length() < 4 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Username should be between 4 and 32 characters");
+        }
+        if (loginService.usernameExists(user.getUsername())) {
+            errors.rejectValue("username", "THis username already exists!");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 6 || user.getPassword().length() > 20) {
+            errors.rejectValue("password", "Password should be between 6 and 20 characters");
+        }
+
+        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+            errors.rejectValue("passwordConfirm", "Passwords do not match!");
+        }
+    }
+}
