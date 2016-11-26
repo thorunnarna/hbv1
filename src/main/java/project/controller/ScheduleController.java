c92ee70cb61c569cd2572578f05d1bedb28dbe9a
svava <svavahb@gmail.com>
2016-11-26 13:53:47 +0000

    commenta schedule controller :calendar:

--- src/main/java/project/controller/ScheduleController.java
+++ src/main/java/project/controller/ScheduleController.java
@@ -32,44 +32,52 @@ import java.util.List;
 @Controller
 public class ScheduleController {
 
-    ScheduleService scheduleService;
-    SearchService searchService;
-    ItemValidator itemValidator = new ItemValidator();
-    //Repository repository
+    // Services for schedules, finding users and validation
+    private ScheduleService scheduleService;
+    private SearchService searchService;
+    private ItemValidator itemValidator = new ItemValidator();
 
-
+    // Constructor
     @Autowired
     public ScheduleController(ScheduleService scheduleService, SearchService searchService){
         this.scheduleService = scheduleService;
         this.searchService = searchService;
-        //this.repository = repository;
     }
 
+    // Get schedule filtered by selectedFilter
     @RequestMapping(value = "/scheduleByFilter")
     public String viewGetScheduleByFilters(Model model, @RequestParam("selectedFilter") String filter){
-
+        // Check if user is logged in
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
 
+        // Find logged in user info
         String tmpUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         User tmpUser = searchService.findByName(tmpUsername);
         int userId = tmpUser.getUserId();
-
-        int yearNow = LocalDateTime.now().getYear();
-        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
-        List <String> TimeSlots = scheduleService.getTimeSlots();
-        List <String> Filters = scheduleService.createfilterList();
         List<User> friendList = tmpUser.getFriends();
         List<Group> groupList = tmpUser.getGroups();
 
+        // Get current week no and year
+        int yearNow = LocalDateTime.now().getYear();
+        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
+
+        // Generate timeslot and filter lists
+        List <String> TimeSlots = scheduleService.getTimeSlots();
+        List <String> Filters = scheduleService.createfilterList();
+
+        // Find scheduleItems that have selectedFilter
         List<ScheduleItem> scheduleByFilters = scheduleService.scheduleItemsFilters(userId, weekNow, yearNow, filter);
+
+        // Add attributes to model
         model.addAttribute("filters",Filters);
         model.addAttribute("timeSlots",TimeSlots);
         model.addAttribute("scheduleItem",new ScheduleItem());
         model.addAttribute("friends", friendList);
         model.addAttribute("groups", groupList);
 
+        // If no filter is selected, redirect to base view
         if(filter.equals("1")) {
             return "redirect:/home";
         }
@@ -78,6 +86,7 @@ public class ScheduleController {
         return "Home";
     }
 
+    // Post method for editing items, not fully implemented
     /*@RequestMapping(value = "/schedule/edit/{itemId}", method = RequestMethod.POST)
     public String editSchedulePost(@ModelAttribute("scheduleItemEdit") ScheduleItem scheduleItem, Model model,
                                    @PathVariable("itemId") int itemId, String title, int userId, LocalDateTime startTime,
@@ -89,32 +98,43 @@ public class ScheduleController {
         return "";
     }*/
 
+    // Method for deleting schedule items
     @RequestMapping(value="/deleteItem")
     public String deleteItemPost(@RequestParam("itemId") int itemId, Model model) {
         scheduleService.removeItem(itemId);
         return "redirect:/home";
     }
 
+    // Method for creating a group
     @RequestMapping(value="/createGroup")
     public String createGroup(@RequestParam("grpName") String grpName, Model model) {
+        // Get logged in user and info
         String LoggedInUser = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = scheduleService.findUserByUsername(LoggedInUser);
         List<User> members = new ArrayList<>();
         members.add(user);
-
-        int yearNow = LocalDateTime.now().getYear();
-        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
-        List <String> Filters = scheduleService.createfilterList();
-        List <String> TimeSlots = scheduleService.getTimeSlots();
         List<User> friendList = user.getFriends();
         List<Group> groupList = user.getGroups();
 
+        // FInd current week no and year
+        int yearNow = LocalDateTime.now().getYear();
+        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
+
+        // Generate timeslot and filter lists
+        List <String> Filters = scheduleService.createfilterList();
+        List <String> TimeSlots = scheduleService.getTimeSlots();
+
+        // Add attributes to model
         model.addAttribute("friends", friendList);
         model.addAttribute("groups", groupList);
         model.addAttribute("filters",Filters);
         model.addAttribute("timeSlots",TimeSlots);
         model.addAttribute("scheduleItem",new ScheduleItem());
+
+        // Add user's schedule items for current week and year
         model.addAttribute("scheduleItems",scheduleService.scheduleItems(user.getUserId(),weekNow,yearNow));
+
+        // If a group with the same name already exists, reload page with error
         if(scheduleService.createGroup(grpName, members)) {
             model.addAttribute("groupFail", false);
             return "redirect:/home";
@@ -124,47 +144,58 @@ public class ScheduleController {
         return "Home";
     }
 
+    // Method for deleting a group
     @RequestMapping(value="/deleteGroup")
     public String deleteGroup(@RequestParam("grpId") int grpId, Model model) {
         scheduleService.deleteGroup(grpId);
         return "redirect:/home";
     }
 
+    // Method for removing a user from the logged in user's friend list
     @RequestMapping(value="/deleteFriendship")
     public String deleteFriendship(@RequestParam("friendId") int friendId, Model model) {
+        // Get logged in user
         String LoggedInUser = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = scheduleService.findUserByUsername(LoggedInUser);
+
+        // Delete the friendship
         scheduleService.deleteFriendship(user.getUserId(), friendId);
         return "redirect:/home";
     }
 
-
+    // Post method for inserting an item into the logged in user's schedule
     @RequestMapping(value = "/home", method = RequestMethod.POST)
     //@PostMapping(value = "/home")
     public String insertItemPost(@ModelAttribute("scheduleItem") ScheduleItem scheduleItem, BindingResult bindingResult,Model model) {
         String newDate="";
+
+        // Find current week no and year
         int yearNow = LocalDateTime.now().getYear();
         int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
+
+        // Get logged in user and info
         String tmpUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         User tmpUser = searchService.findByName(tmpUsername);
         int userid = tmpUser.getUserId();
-
-
-        List <String> TimeSlots = scheduleService.getTimeSlots();
-        model.addAttribute("timeSlots",TimeSlots);
-
         List<User> friendList = tmpUser.getFriends();
         List<Group> groupList = tmpUser.getGroups();
+
+
+        // Generate timeslot and filter lists
+        List <String> TimeSlots = scheduleService.getTimeSlots();
+        List <String> Filters = scheduleService.createfilterList();
+
+        // Add attributes to model
+        model.addAttribute("timeSlots",TimeSlots);
         model.addAttribute("friends", friendList);
         model.addAttribute("groups", groupList);
-
-        List <String> Filters = scheduleService.createfilterList();
         model.addAttribute("filters",Filters);
 
-
+        // Validate user input if the date is empty
         if (scheduleItem.getdate() == ""){
             bindingResult.rejectValue("date","You must choose a date");
             itemValidator.validate(scheduleItem, bindingResult);
+
             if (bindingResult.hasErrors()) {
                 model.addAttribute("scheduleItems",scheduleService.scheduleItems(userid,weekNow,yearNow));
                 model.addAttribute("hasErrors", true);
@@ -172,27 +203,28 @@ public class ScheduleController {
                 return "Home";
             }
         }
+        // else change the date string into a new date string, for further parsing
         else {newDate = scheduleService.changeStringDateToRigthDate(scheduleItem.getdate());}
 
+        // Change format of start and end time strings
         String newSTime = scheduleService.changeformatOfTime(scheduleItem.getStartstring());
         String newETime = scheduleService.changeformatOfTime(scheduleItem.getEndstring());
 
+        // Create whole datetime strings for the start and end dates
         String startTimeforItem = newDate +" "+ newSTime;
         String endTimeforItem = newDate +" "+newETime;
 
+        // Format the strings to LocalDateTime
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime startdateTime = LocalDateTime.parse(startTimeforItem,formatter);
         LocalDateTime enddateTime = LocalDateTime.parse(endTimeforItem,formatter);
-
         scheduleItem.setStartTime(startdateTime);
         scheduleItem.setEndTime(enddateTime);
 
-
-
-
-
+        // Validate the rest of the input
         itemValidator.validate(scheduleItem, bindingResult);
 
+        // If there are errors, reload page with errors
         if (bindingResult.hasErrors()) {
             model.addAttribute("scheduleItems",scheduleService.scheduleItems(userid,weekNow,yearNow));
             model.addAttribute("hasErrors", true);
@@ -200,41 +232,51 @@ public class ScheduleController {
             return "Home";
         }
 
+        // Find week no and year of the new item
         int year = scheduleService.findYear(scheduleItem.getdate());
         int weekNo = scheduleService.findWeekNo(startdateTime);
 
+        // Create the new item
          scheduleService.createItem(scheduleItem.getTitle(), userid, startdateTime, enddateTime,
                 scheduleItem.getTaggedUsers(), weekNo, year, scheduleItem.getLocation(),
                 scheduleItem.getColor(),scheduleItem.getDescription(), scheduleItem.getFilter());
 
-        //hreinsa breytur fyrir næsta item
+        // Clean variables so the input values are empty, for next insertion
         ScheduleItem scheduleitem = new ScheduleItem();
         model.addAttribute("scheduleItem",scheduleitem);
+
+        // Add to model attributes for logged in user, current week no and year
         model.addAttribute("scheduleItems",scheduleService.scheduleItems(userid,weekNow,yearNow));
 
         return "Home";
     }
 
+    // Get base home page
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(Model model) {
+        // Check if user is logged in
         boolean isLoggedIn;
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
         else isLoggedIn = true;
+
+        // Get logged in user and info
         String LoggedInUser = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        List <String> TimeSlots = scheduleService.getTimeSlots();
-
-        int yearNow = LocalDateTime.now().getYear();
-        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
         User user = scheduleService.findUserByUsername(LoggedInUser);
         int userid= user.getUserId();
-
-        List <String> Filters = scheduleService.createfilterList();
         List<User> friendList = user.getFriends();
         List<Group> groupList = user.getGroups();
 
+        // Find the current week no and year
+        int yearNow = LocalDateTime.now().getYear();
+        int weekNow = scheduleService.findWeekNo(LocalDateTime.now());
+
+        // Generate filter and timeslot lists
+        List <String> TimeSlots = scheduleService.getTimeSlots();
+        List <String> Filters = scheduleService.createfilterList();
+
+        // Add attributes to model
         model.addAttribute("filters",Filters);
         model.addAttribute("friends", friendList);
         model.addAttribute("groups", groupList);
@@ -244,9 +286,6 @@ public class ScheduleController {
         model.addAttribute("scheduleItem", new ScheduleItem());
         model.addAttribute("scheduleItems",scheduleService.scheduleItems(userid,weekNow,yearNow));
         model.addAttribute("groupFail", false);
-        //model.addAttribute("date",date);
-        //model.addAttribute("sTime",sTime);
-        //model.addAttribute("eTime",eTime);
 
         return "Home";
     }
