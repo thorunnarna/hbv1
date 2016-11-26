6c0eaafe0ff0788a12b39b17da9b792c40073fea
svava <svavahb@gmail.com>
2016-11-26 13:59:55 +0000

    commenta search controller :mag:

--- src/main/java/project/controller/SearchController.java
+++ src/main/java/project/controller/SearchController.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 
 @Controller
 public class SearchController {
+
+    // Class for sending over user info + info on whether the user is the logged in user's friend
     public class UserHolder {
         User user;
         boolean friendship;
@@ -31,25 +33,34 @@ public class SearchController {
         }
     }
 
+    // Service for search business logic
     SearchService searchService;
 
+    // Constructor
     @Autowired
     public SearchController(SearchService searchService){
         this.searchService = searchService;
     }
 
+    // Get base list of all users
     @RequestMapping(value="/search")
     public String viewGetListOfUsers(Model model){
+        // CHeck if user is logged in
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
+
+        // Get logged in user and info
         String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         User loggedInUser = searchService.findByName(loggedInUsername);
         List<User> users = searchService.findAll();
         List<UserHolder> userHolders = new ArrayList<>();
         List<Group> groups = loggedInUser.getGroups();
+
         model.addAttribute("groupList", groups);
         model.addAttribute("loggedInId",loggedInUser.getUserId());
+
+        // Generate user holders for all users
         for(User user:users ) {
             UserHolder uh = new UserHolder();
             uh.user = user;
@@ -61,17 +72,25 @@ public class SearchController {
         return "Search";
     }
 
+    // Method for searching for a certain username
     @RequestMapping(value="/search/q")
     public String getSearchByName(@RequestParam("username") String username, Model model){
+        // Check if user is logged in
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
+
+        // Get logged in user and info
         String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         User loggedInUser = searchService.findByName(loggedInUsername);
+
+        //Find user searched for
         User user = searchService.findByName(username);
         if(user.getUsername() == null) {
             return "Search";
         }
+
+        // Generate necessary variables for model
         boolean friendship = searchService.checkIfFriend(user, loggedInUser);
         List<Group> groups = loggedInUser.getGroups();
         model.addAttribute("groupList", groups);
@@ -86,29 +105,43 @@ public class SearchController {
         return "Search";
     }
 
+    // Post method for adding a user as the logged in user's friend
     @RequestMapping(value="/search/addFriend", method = RequestMethod.POST)
     public String addFriendPost(@RequestParam("userId") int userId) {
+        // Check if user is logged in
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
+        // Find logged in user and info
         String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
         User loggedInUser = searchService.findByName(loggedInUsername);
+
+        // Find user selected for adding as friend
         User user2 = searchService.findByUserId(userId);
+
+        // If they are not already friends, create the friendship
         if(!searchService.checkIfFriend(loggedInUser, user2)) {
             searchService.createFriendship(loggedInUser.getUserId(), userId);
         }
         return "redirect:/search";
     }
 
+    // Add a user to a certain group
     @RequestMapping(value="/search/addToGroup", method = RequestMethod.POST)
     public String addToGroup(@RequestParam("addToGroup") int grpId, @RequestParam("addMember") int userId) {
+        // Check if user is logged in
         if (SecurityContextHolder.getContext().getAuthentication() == null ) {
             return "redirect:/";
         }
+
+        // Find the group and the user
         Group group = searchService.findGroup(grpId);
         User user = searchService.findByUserId(userId);
+
+        // Add the user to the group
         searchService.addGroupMemeber(grpId, userId);
         group.addMember(user);
+        
         return "redirect:/search";
     }
 }
